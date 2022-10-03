7646a074c38f298c099b8010e831ccc24977aec0
MarcoVvenicio <71838840+MarcoVvenicio@users.noreply.github.com>
2022-10-03 19:31:01 -0300

    Delete App.js

--- App.js
+++
@@ -1,9 +0,0 @@
-let armazenamento = {};
-
-function dados(){
-    armazenamento.nome = document.getElementById("nome").value;
-    armazenamento.numero = document.getElementById("numero").value;
-    armazenamento.motivo = document.getElementById("motivo").value;
-
-}
-console.log(armazenamento);
